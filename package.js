9d39bb48e8a3a8bdcd44291c79e763371d613b09
Angle <Ribbonwing@gmail.com>
2018-04-29 18:49:03 -0600

    add base code for activityRequest, and package.js

---
+++ package.js
@@ -0,0 +1,39 @@
+/*
+    Javascript ActivityPub Software
+    Copyright (C) 2018 Gregory Sartucci
+    License: AGPL-3.0, Check file LICENSE
+*/
+
+Package.describe({
+    name: 'agoraforum:JSActivityPub',
+    version: '0.0.1',
+    summary: 'ActivityPub Library',
+    git: 'https://github.com/Agora-Project/JSActivityPub',
+    documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+    both = ['client', 'server'];
+
+    api.addFiles([
+        'activityRequest.js'
+    ], 'server');
+
+    api.addFiles([
+    ], both);
+
+    api.use([
+        'timbrandin:fetch'
+    ], both);
+
+    api.addFiles([
+    ], 'client');
+
+    api.use([
+    ], 'client');
+});
+
+Package.onTest(function(api) {
+    api.use('tinytest');
+    api.use('agoraforum:JSActivityPub', ['client', 'server']);
+});
